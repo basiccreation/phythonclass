32438c24ba2114a4d35895badb68f7ff1432f257
Pia <basiccreation@yahoo.com>
2019-08-07 23:58:21 -0700

    added print out to file

--- PY_ASSIGNMENT_10.py
+++ PY_ASSIGNMENT_10.py
@@ -71,12 +71,6 @@ class ProductionWorker(Employee):
 
         # Initialize __shift_number attribute
         self.__hourly_pay_rate = hourly_pay_rate
-        
-        # Initialize __employee_name attribute
-        # self.__employee_name = employee_name
-
-        # Initialize __employee_number attribute
-        # self.__employee_number = employee_number
 
     # Set mutators
     def set_shift_number(self, shift_number):
@@ -96,88 +90,141 @@ class ProductionWorker(Employee):
         if self.__shift_number == 1:
             return "Day Shift"
         if self.__shift_number == 2:
-            return "Night Shift"    
+            return "Night Shift"
+
+    def formatted_rate(self):
+        formatted = float(self.__hourly_pay_rate)
+        formatted = "%.2f" % formatted 
+        return formatted
 
     # Set __set__
     def __str__(self):
-        return "\n\nYou entered:\n------------------" +\
+        return "\n\nYou entered:" +\
+               "\n------------------" +\
                "\nEmployee Name: " + str(self._employee_name) +\
                "\nEmployee Number: " + str(self._employee_number) +\
-               "\nWork Shift: " + str(self.__shift_number) + " - " + \
-               self.shift_description() +\
-               "\nPay Rate $: " +\
-               format(self.__hourly_pay_rate, ',.2f') +\
+               "\nWork Shift: " + str(self.__shift_number) + " - " +\
+                                      self.shift_description() +\
+               "\nPay Rate $: " + self.formatted_rate() +\
                "\n------------------"
     
 # main() contains programs main     
 def main():
-
-    # Ask for user input
-    print("Enter Employee information below:")
-
-    ''' Get Worker Data'''
-    # Get Name
-    name = input("Name: ")
-
-    # Verify name is at least two entries long
-    while len(name.strip().split()) < 2:
-        name = input("Enter full name: ")
-
-    # Get employee number
-    number = int(input("Number: "))
-
-    # Verify employee number is 3 digits
-    while number < 100 or number > 999:
-        number = int(input("Employee number is 3 digits: "))    
-
-    # Get shift
-    shift = int(input("Shift: "))
-
-    # Verify it is either 1 or 2
-    while shift < 0 or shift > 2:
-        shift = int(input("Enter 1 for Day and 2 for Night: "))
-
-    # Get pay rate
-    rate = float(input("Rate: "))
-
-    # Verify the amount is either regular pay, plus 50% or double
-    while rate not in [20.20,30.30,40.40]:
-        #print("Check the rate again.\n- Regular hours: $20.20")
-        #print("- Plus 50%: $30.30\n- double: $40.40")
-        print("Check the rate again.")
-        print("- Regular:  $20.20")
-        print("- Plus 50%: $30.30")
-        print("- Double:   $40.40")
-        rate = float(input("> "))
-
-    # Create object from ProductionWorker class
-    worker_entry = ProductionWorker(name, number, shift, rate)
-    '''   
-    # Display the entered data
-    print("\n\nYou entered:\n------------------")
     
-    print("Employee Name:",
-          worker_entry.get_employee_name())
-    
-    print("Employee Number:",
-          worker_entry.get_employee_number())
-    
-    print("Work Shift:",
-          worker_entry.get_shift_number(),"-",
-          worker_entry.shift_description())
-    
-    print("Pay Rate: $",
-          format(worker_entry.get_hourly_pay_rate(), ',.2f'), sep='')
-    
-    print("------------------")
-    '''
-    print(worker_entry)
-    
+    # Boolean variable y/n
+    enter_another = "y" 
+
+    # Keep entering another until user says no
+    while enter_another == "y":
+
+        # Ask for user input
+        print("Enter Employee information below:")
+
+        ''' Get Worker Data'''
+        # Get Name
+        name = input("Name: ")
+
+        # Verify name is at least two entries long
+        while len(name.strip().split()) < 2:
+            name = input("Enter full name:\n> ")
+
+        # Get employee number
+        number = input("Number: ")
+        
+        # Verify number is a digit
+        if number.isdigit():
+            
+            # Convert number to integer
+            number = int(number)
+            
+            # Verify number is in range
+            while int(number) < 100 or int(number) > 999:
+                number = int(input("Employee number is 3 digits:\n> "))
+
+        # Error message if number is alpha or alpha-numeric 
+        elif number.isalpha() or number.isalnum():
+            number = int(input("Employee number is 3 digits:\n> "))
+            
+            # Verify number is in range
+            while int(number) < 100 or int(number) > 999:
+                number = int(input("Employee number is 3 digits:\n> "))
+                
+        # Get shift
+        shift = input("Shift: ")
+
+        # If shift is a digit, change shift to an integer
+        if shift.isdigit():
+            shift = int(shift)
+
+            # Verify it is either 1 or 2
+            while shift < 0 or shift > 2:
+                shift = int(input("Enter 1 for Day and 2 for Night:\n> "))
+
+        # Error message if shift is alpha or alpha-numeric             
+        elif shift.isalpha() or shift.isalnum():
+            shift = int(input("Enter 1 for Day and 2 for Night:\n> "))
+                        
+        # Get pay rate
+        rate = input("Rate: ")
+        
+        # if rate is a digit, change rate to a float
+        if rate.isdigit():
+            rate = float(rate)
+            
+            # Verify the amount is in range $20 - $30
+            while rate < 20 or rate > 30:
+                
+                # Error message if rate is lower than 20 or higher than 40
+                print("Check the rate again.")
+                print("Rate should be between $20 and $40")
+                rate = float(input("> "))
+                
+        # Error message if shift is alpha or alpha-numeric      
+        elif rate.isalpha() or rate.isalnum():
+            print("Check the rate again.")
+            print("Rate should be between $20 and $40")
+            rate = float(input("> "))
+            
+        # Create object from ProductionWorker class
+        worker_entry = ProductionWorker(name, number, shift, rate)
+
+        # Print the entered information
+        print(worker_entry)
+
+        # Ask user if they would like to enter another employee
+        enter_another = input("Enter another? y/n\n> ")
 
 main()
 '''
 -------
 OUTPUT
 -------
+===== RESTART: /Users/piasmith/Desktop/python_class/PY_ASSIGNMENT_10.py =====
+Enter Employee information below:
+Name: Mads
+Enter full name:
+> Mads Mikkelsen
+Number: 2
+Employee number is 3 digits:
+> 234
+Shift: night
+Enter 1 for Day and 2 for Night:
+> 2
+Rate: five
+Check the rate again.
+Rate should be between $20 and $40
+> 23.45
+
+
+You entered:
+------------------
+Employee Name: Mads Mikkelsen
+Employee Number: 234
+Work Shift: 2 - Night Shift
+Pay Rate $: 23.45
+------------------
+Enter another? y/n
+> n
+>>>
 
 '''
